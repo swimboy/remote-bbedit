a867feb75b77a347f727b8dd473caeaa2b885bb5
swimboy <github@jeff.tagcomp.com>
2019-11-16 08:45:51 -0500

    Modified control sequence monitor to use wrapper

--- Remote-BBEdit.py
+++ Remote-BBEdit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 import asyncio
 import iterm2
@@ -6,10 +6,12 @@ import os
 
 async def main(connection):
     app = await iterm2.async_get_app(connection)
-    async with iterm2.CustomControlSequenceMonitor(connection, "remote-bbedit", r'.*') as mon:
-        while True:
-            match = await mon.async_get()
-            session = app.current_terminal_window.current_tab.current_session
-            os.system("/usr/local/bin/bbedit --wait --resume -- " + match.string)
-            await session.async_send_text("Y")
+    async def bbeditWrapper():
+        async with iterm2.CustomControlSequenceMonitor(connection, "remote-bbedit", r'.*') as mon:
+            while True:
+                match = await mon.async_get()
+                session = app.current_terminal_window.current_tab.current_session
+                os.system("/usr/local/bin/bbedit --wait --resume -- " + match.string)
+                await session.async_send_text("Y")
+    asyncio.create_task(bbeditWrapper())
 iterm2.run_forever(main)
