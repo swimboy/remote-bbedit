052c0bc7f00bafabd405bc3d15a9bd691ce948d1
swimboy <github@jeff.tagcomp.com>
2019-11-21 15:33:36 -0500

    Uses EachSessionOnceMonitor to create a separate thread for each session

--- Remote-BBEdit.py
+++ Remote-BBEdit.py
@@ -6,12 +6,13 @@ import os
 
 async def main(connection):
     app = await iterm2.async_get_app(connection)
-    async def bbeditWrapper():
-        async with iterm2.CustomControlSequenceMonitor(connection, "remote-bbedit", r'.*') as mon:
+    async def watch(session_id):
+        session = app.get_session_by_id(session_id)
+        async with iterm2.CustomControlSequenceMonitor(connection, "remote-bbedit", r'.*', session_id) as mon:
             while True:
                 match = await mon.async_get()
-                session = app.current_terminal_window.current_tab.current_session
                 os.system("/usr/local/bin/bbedit --wait --resume -- " + match.string)
                 await session.async_send_text("Y")
-    asyncio.create_task(bbeditWrapper())
+    await iterm2.EachSessionOnceMonitor.async_foreach_session_create_task(app, watch)
+
 iterm2.run_forever(main)
